c0a7e85cef8c9f1552a74dca89f0d85be95f289a
danielmamian99 <danimaster99@gmail.com>
2022-10-04 23:38:04 -0500

    priorizando la composicion antes que la herencia

---
+++ src/clean-code/06-composicion-clases.ts
@@ -0,0 +1,222 @@
+//! Se dice que debemos priorizar la composición sobre la herencia
+//! Ya que la herencia rompe el principio de responsabilidad unica 
+(() => {
+    // priorizando la herencia
+    // No aplicando el principio de responsabilidad única
+
+    type Gender = 'M'|'F';
+
+    interface PersonProps {
+        birthdate : Date;
+        gender    : Gender;
+        name      : string;
+    }
+
+    class Person {
+        public birthdate: Date;
+        public gender   : Gender;
+        public name     : string;
+
+        constructor({ name, gender, birthdate }: PersonProps ){
+            this.name      = name;
+            this.gender    = gender;
+            this.birthdate = birthdate;
+        }
+    }
+
+
+    interface UserProps {
+        birthdate : Date;
+        email     : string;
+        gender    : Gender;
+        name      : string;
+        role      : string;
+    }
+
+    class User extends Person {
+        
+        public email: string;
+        public role : string;
+        public lastAccess: Date;
+
+        constructor({
+            birthdate,
+            email,
+            gender,
+            name,
+            role,
+        }: UserProps ) {
+            super({ name, gender, birthdate });
+            this.lastAccess = new Date();
+            this.email = email;
+            this.role  = role;
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    interface UserSettingsProps {
+        birthdate        : Date;
+        email            : string;
+        gender           : Gender;
+        lastOpenFolder   : string;
+        name             : string;
+        role             : string;
+        workingDirectory : string;
+    }
+
+    class UserSettings extends User {
+
+        public workingDirectory: string;
+        public lastOpenFolder  : string;
+
+        constructor({
+            workingDirectory,
+            lastOpenFolder,
+            email,
+            role,
+            name,
+            gender,
+            birthdate,
+        }: UserSettingsProps ) {
+            super({ email, role, name, gender, birthdate });
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder   = lastOpenFolder;
+        }
+    }
+
+
+    const userSettings = new UserSettings({
+        birthdate: new Date('1985-10-21'),
+        email: 'fernando@google.com',
+        gender: 'M',
+        lastOpenFolder: '/home',
+        name: 'Fernando',
+        role: 'Admin',
+        workingDirectory: '/usr/home',
+    });
+
+    console.log({ userSettings });
+
+
+})();
+
+(() => {
+
+    // Aplicando el principio de responsabilidad única
+    // Priorizar la composición frente a la herencia!
+
+    type Gender = 'M'|'F';
+
+    interface PersonProps {
+        birthdate : Date;
+        gender    : Gender;
+        name      : string;
+    }
+
+    class Person {
+        public birthdate: Date;
+        public gender   : Gender;
+        public name     : string;
+
+        constructor({ name, gender, birthdate }: PersonProps ){
+            this.name      = name;
+            this.gender    = gender;
+            this.birthdate = birthdate;
+        }
+    }
+
+
+    interface UserProps {
+        email     : string;
+        role      : string;
+    }
+
+    class User {
+        
+        public email      : string;
+        public lastAccess : Date;
+        public role       : string;
+
+        constructor({
+            email,
+            role,
+        }: UserProps ) {
+            this.lastAccess = new Date();
+            this.email = email;
+            this.role  = role;
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    interface SettingsProps {
+        lastOpenFolder   : string;
+        workingDirectory : string;
+    }
+
+    class Settings {
+
+        public workingDirectory: string;
+        public lastOpenFolder  : string;
+
+        constructor({
+            lastOpenFolder,
+            workingDirectory,
+        }: SettingsProps ) {
+            this.lastOpenFolder   = lastOpenFolder;
+            this.workingDirectory = workingDirectory;
+        }
+    }
+
+
+    interface UserSettingsProps {
+        birthdate        : Date;
+        email            : string;
+        gender           : Gender;
+        lastOpenFolder   : string;
+        name             : string;
+        role             : string;
+        workingDirectory : string;
+    }
+
+    class UserSettings {
+
+        public person  : Person;
+        public user    : User;
+        public settings: Settings;
+
+        constructor({
+            name, gender, birthdate,
+            email, role,
+            lastOpenFolder, workingDirectory,
+        }: UserSettingsProps ){
+
+            this.person = new Person({ name, gender, birthdate });
+            this.user = new User({ email, role });
+            this.settings = new Settings({ lastOpenFolder, workingDirectory })
+        }
+    }
+
+
+
+    const userSettings = new UserSettings({
+        birthdate: new Date('1985-10-21'),
+        email: 'fernando@google.com',
+        gender: 'M',
+        lastOpenFolder: '/home',
+        name: 'Fernando',
+        role: 'Admin',
+        workingDirectory: '/usr/home',
+    });
+
+    console.log({ userSettings });
+
+    
+})();
